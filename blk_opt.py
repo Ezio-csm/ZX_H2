32b8d088b4d65350e5025561aba423fa1cad505d
Ezio_csm <Ezio_csm@qq.com>
2024-07-03 18:30:55 +0800

    Add blackbox optimization for SSM lags

--- blk_opt.py
+++ blk_opt.py
@@ -1,5 +1,6 @@
 import json
 import copy
+import argparse
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
@@ -7,14 +8,22 @@ import plotly.graph_objects as go
 import plotly.io as pio
 from plotly.subplots import make_subplots
 
-config_file_path = './configs/TIC1141.json'
+parser = argparse.ArgumentParser()
+parser.add_argument("config", type=str, help="config file")
+args = parser.parse_args()
+
+config_file_path = args.config
 
 with open(config_file_path, 'r') as file:
     config = json.load(file)
+
 data_path = config['Data_path']
-train_size = config['Training_size'] + config['Test_size']
 data = pd.read_csv(data_path)
-data = data[:train_size]
+
+if config['Test_size'] == -1:
+    config['Test_size'] = data.shape[0] - config['Training_size']
+spilt_size = config['Training_size'] + config['Test_size']
+data = data[:spilt_size]
 data['Time'] = range(data.shape[0])
 data = data[['Time'] + config['MV_name'] + config['CV_name']]
 
@@ -42,15 +51,15 @@ space.add_variables(sp_lags)
 def opt_wrapper(config):
     lags = [config[f'lag{i}'] for i in range(numv)]
     model_config = config_setter(lags=lags)
-    model = SMMKalmanFilters(data, config=model_config)
-    error, _, _ = model.forward()
-    return {'objectives': [error]}
+    model = SMMKalmanFilters(config=model_config)
+    rmse, _, _ = model.fit(data)
+    return {'objectives': [rmse]}
 
 # Run
 if __name__ == '__main__':
     opt = ParallelOptimizer(opt_wrapper, space,
                     parallel_strategy='async',
-                    batch_size=8,
+                    batch_size=16,
                     batch_strategy='default',
                     num_objectives=1,
                     num_constraints=0,
