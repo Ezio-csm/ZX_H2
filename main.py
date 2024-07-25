73b45241cb79dc998a27a7195bebdd57571ff437
Ezio_csm <Ezio_csm@qq.com>
2024-07-25 14:08:17 +0800

    Add new args for main

--- main.py
+++ main.py
@@ -10,24 +10,29 @@ import plotly.graph_objects as go
 import plotly.io as pio
 from plotly.subplots import make_subplots
 
-# from pyinstrument import Profiler
+from pyinstrument import Profiler
 
 parser = argparse.ArgumentParser()
 parser.add_argument("config", type=str, help="config file")
+parser.add_argument("output", type=str, help="output arg")
 args = parser.parse_args()
 
 config_file_path = args.config
+output_arg = args.output
 
 with open(config_file_path, 'r') as file:
     config = json.load(file)
 
 # ---------------------
-# profiler = Profiler()
-# profiler.start()
+profiler = Profiler()
+profiler.start()
 # ---------------------
 
 data_path = config['Data_path']
 data = pd.read_csv(data_path)
+
+if output_arg != None:
+    config['CV_name'] = [output_arg]
 
 if config['Test_size'] == -1:
     config['Test_size'] = data.shape[0] - config['Training_size']
@@ -50,6 +55,8 @@ data = data[['Time'] + config['MV_name'] + config['CV_name']]
 from SSM import SMMKalmanFilters
 
 if __name__ == '__main__':
+    if output_arg != None:
+        print(f"CV_name changed : {config['CV_name']}")
     model = SMMKalmanFilters(config=config)
     if config['ModelLoaded']:
         with open(config['ModelLoadedPath'], 'rb') as f:
@@ -61,7 +68,10 @@ if __name__ == '__main__':
     results = model.forecast(data)
     if not os.path.exists('./results'):
         os.makedirs('./results')
-    results.to_csv('./results/results.csv')
+    if output_arg != None:
+        results.to_csv(f'./results/{output_arg}.csv')
+    else:
+        results.to_csv('./results/results.csv')
 
     if config['ModelSaved']:
         if not os.path.exists(os.path.dirname(config['ModelSavedPath'])):
@@ -75,6 +85,6 @@ if __name__ == '__main__':
 
 
 # ---------------------
-# profiler.stop()
-# print(profiler.output_text(unicode=True, color=True))
+profiler.stop()
+print(profiler.output_text(unicode=True, color=True))
 # ---------------------
